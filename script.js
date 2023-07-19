a4a4679ab576f3d99dda948f4fc02d996c87a233
N4eem4 <naeemahassan439@gmail.com>
2023-07-19 11:44:28 -0400

    message here

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("links");
+    if (x.style.display === "flex") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }
+  
+  
