ce3b2510d5da9c4adaae63bfacbcdfe51cb77841
N4eem4 <naeemahassan439@gmail.com>
2024-09-05 14:41:14 -0400

    changed the navbar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function myFunction() {
-  var x = document.getElementById("links");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+  var link = document.getElementById("links");
+  if (link.style.display === "flex") {
+    link.style.display = "none";
   } else {
-    x.style.display = "flex";
+    link.style.display = "flex";
   }
 }
 
