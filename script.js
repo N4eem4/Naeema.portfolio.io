642f2b1d5dadce1d0546a52c47c0ee4415088c49
N4eem4 <naeemahassan439@gmail.com>
2024-12-18 14:30:50 -0500

    fixed navbar issues and added some functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function myFunction() {
+function navHide() {
+  var w = window.innerWidth;
   var link = document.getElementById("links");
-  if (link.style.display === "flex") {
+  if (link.style.display === "flex" && w <= 1000) {
     link.style.display = "none";
   } else {
     link.style.display = "flex";
+  }
+}
+
+function resize() {
+  var w = window.innerWidth;
+  if (w >= 1001) {
+    document.getElementById('links').style.display = 'flex';
   }
 }
 
