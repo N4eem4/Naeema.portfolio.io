992c660c237f1e1926a8cbf6952f1bb560d0893d
N4eem4 <naeemahassan439@gmail.com>
2025-01-27 14:46:23 -0500

    redesign navbar pt 2

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 function closeNav() {
   var w = window.innerWidth;
+  var nav = document.querySelectorAll("[id='navlinks']");
   if(w < 1000){
     document.getElementById("links").style.width = "0";
+    for(var i = 0; i < nav.length; i++){
+      nav[i].style.display = 'none';
+     }
   }
   
 }
 
 function resize() {
   var w = window.innerWidth;
+  var nav = document.querySelectorAll("[id='navlinks']");
   if (w >= 1001) {
     document.getElementById('links').style.width = '100%';
+   for(var i = 0; i < nav.length; i++){
+    nav[i].style.display = 'flex';
+   }
   }
   else if (w< 1000){
     document.getElementById('links').style.width = "0";
+    for(var i = 0; i < nav.length; i++){
+      nav[i].style.display = 'none';
+     }
   }
 }
 
 function openNav(){
+  var nav = document.querySelectorAll("[id='navlinks']");
   if(window.innerWidth < 1000)
     {
       document.getElementById("links").style.width = "300px";
+      for(var i = 0; i < nav.length; i++){
+        nav[i].style.display = 'flex';
+       }
     }
   
 }
